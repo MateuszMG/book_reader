8cfbfc0d6232e5ac6ebbb222ca67451a24659221
name <email>
2024-09-14 09:34:51 +0200

    add SpeechSynthesisService

---
+++ src/services/speach.ts
@@ -0,0 +1,57 @@
+export class SpeechSynthesisService {
+  utterance: SpeechSynthesisUtterance;
+  selected: SpeechSynthesisVoice;
+
+  constructor(props: {
+    voice: string;
+    text: string;
+    lang: string;
+    pitch: string;
+    rate: string;
+    volume: string;
+  }) {
+    this.utterance = new SpeechSynthesisUtterance();
+    this.selected = SpeechSynthesisService.getVoice(props.voice);
+    this.utterance.voice = this.selected;
+    this.utterance.text = props.text.replace(/\n/g, "");
+    this.utterance.lang = props.lang || "en-EN";
+    this.utterance.pitch = parseFloat(props.pitch) || 0.8;
+    this.utterance.rate = parseFloat(props.rate) || 1;
+    this.utterance.volume = parseFloat(props.volume) || 1;
+  }
+
+  static supported() {
+    return window.speechSynthesis;
+  }
+
+  static getVoice(selected: SpeechSynthesisVoice["name"]) {
+    const voices = window.speechSynthesis.getVoices();
+    const voice = voices.find((voice) => voice.name === selected);
+    return voice !== undefined ? voice : voices[0];
+  }
+
+  onend(func: () => {}) {
+    this.utterance.onend = func;
+  }
+
+  onerror(func: () => {}) {
+    this.utterance.onerror = func;
+  }
+
+  speak() {
+    window.speechSynthesis.cancel();
+    window.speechSynthesis.speak(this.utterance);
+  }
+
+  pause() {
+    window.speechSynthesis.pause();
+  }
+
+  cancel() {
+    window.speechSynthesis.cancel();
+  }
+
+  resume() {
+    window.speechSynthesis.resume();
+  }
+}
